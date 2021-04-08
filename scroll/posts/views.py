8ec84424394f14bf27228d02dda24944641557a4
astafanous <astafanousf@yahoo.com>
2021-04-08 11:41:43 +0200

    add all lecture 6

--- scroll/posts/views.py
+++ scroll/posts/views.py
@@ -4,8 +4,11 @@ from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
     return render(request, "posts/index.html")
+
 
 def posts(request):
 
